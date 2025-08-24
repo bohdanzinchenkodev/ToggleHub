25026827960c26199eca2d4ad566b1a5a95e5c6c
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2025-08-23 20:17:57 -0600

    Add UserController; implement GetUser endpoint for user retrieval

---
+++ src/ToggleHub.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using ToggleHub.Application.Interfaces;
+
+namespace ToggleHub.API.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        if (user == null)
+            return NotFound("User not found.");
+
+        return Ok(user);
+    }
+}
