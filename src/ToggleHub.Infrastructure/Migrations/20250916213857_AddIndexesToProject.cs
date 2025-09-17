b5c936ceb80e9f8fc08b5051e23b1103bf0224cc
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2025-09-17 15:33:52 -0600

    Add composite index for `Projects` on `OrganizationId` and `Slug`. Update related foreign key constraints for `Environments`.

---
+++ src/ToggleHub.Infrastructure/Migrations/20250916213857_AddIndexesToProject.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToggleHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Environments_Projects_ProjectId",
+                table: "Environments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_OrganizationId",
+                table: "Projects");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_OrganizationId_Slug",
+                table: "Projects",
+                columns: new[] { "OrganizationId", "Slug" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Environments_Projects_ProjectId",
+                table: "Environments",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Environments_Projects_ProjectId",
+                table: "Environments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_OrganizationId_Slug",
+                table: "Projects");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_OrganizationId",
+                table: "Projects",
+                column: "OrganizationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Environments_Projects_ProjectId",
+                table: "Environments",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
