c0733cdf5def8be15ebc64ee08ff652e80a93247
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2025-09-29 21:34:01 -0600

    Remove discriminator from `AspNetRoles` to simplify role mapping structure.

--- src/ToggleHub.Infrastructure.Identity/Migrations/20250930032847_RemoveDescriminatorFromRole.Designer.cs
+++ src/ToggleHub.Infrastructure.Identity/Migrations/20250930032847_RemoveDescriminatorFromRole.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToggleHub.Infrastructure.Identity.Data;
 
@@ -11,9 +12,11 @@ using ToggleHub.Infrastructure.Identity.Data;
 namespace ToggleHub.Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(ToggleHubIdentityDbContext))]
-    partial class ToggleHubIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250930032847_RemoveDescriminatorFromRole")]
+    partial class RemoveDescriminatorFromRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ToggleHub.Infrastructure.Identity/Migrations/20250930032847_RemoveDescriminatorFromRole.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToggleHub.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDescriminatorFromRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetRoles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetRoles",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
